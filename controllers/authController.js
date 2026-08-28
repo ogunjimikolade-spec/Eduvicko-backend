eec59e40de4b7861cc22d3983c8d546b2b097808
kolade <ogunjimikolade@gmail.com>
2026-08-28 18:39:10 +0300

    remove role from register

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,7 +13,7 @@ exports.register = async (req, res, next) => {
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const user = await User.create({ name, email, password: hashedPassword, role });
+    const user = await User.create({ name, email, password: hashedPassword });
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '30d' });
 
