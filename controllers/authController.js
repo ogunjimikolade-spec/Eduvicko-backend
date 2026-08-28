c7b6fd021984db9d9f051db29810af355c28e0d0
kolade <ogunjimikolade@gmail.com>
2026-08-28 21:45:35 +0300

    update register

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcryptjs');
 
 console.log('bcrypt loaded:', typeof bcrypt.hash); // <-- ADD THIS LINE
 
-exports.register = async (req, res, next) => {
+exports.register = async (req, res) => {
   try {
     const { name, email, password, role } = req.body;
 
@@ -13,17 +13,28 @@ exports.register = async (req, res, next) => {
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const user = await User.create({ name, email, password: hashedPassword , role });
+    const user = await User.create({ 
+      name, 
+      email, 
+      password: hashedPassword, 
+      role 
+    });
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '30d' });
 
-    res.status(201).json({ _id: user._id, name: user.name, email: user.email, role: user.role, token });
-    
+    res.status(201).json({ 
+      _id: user._id, 
+      name: user.name, 
+      email: user.email,
+      role: user.role,
+      token 
+    });
+
   } catch (error) {
-    console.log('FULL ERROR:', error);
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ message: error.message })
   }
-};
+}
+
 exports.login = async (req, res) => {
   try {
     const { email, password } = req.body;
