5335c2d76eb4c582e3d3e4dde28c7e4136d15ed8
kolade <ogunjimikolade@gmail.com>
2026-08-30 15:32:38 +0300

    fix cors for vercel frontend

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: ["http://localhost:5173", "https://eduvicko-backend.onrender.com"],
+  origin: ["http://localhost:5173", "https://eduvicko-frontend.vercel.app"],
   credentials: true
 }));
 
